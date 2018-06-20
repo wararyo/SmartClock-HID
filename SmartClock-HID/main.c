e4fdd5b92e4f21deda58f26acfb1648c460bf7a0
wararyo <wararyo@gmail.com>
2018-06-21 02:11:10 +0900

    とりあえず動作するようになった

--- SmartClock-HID/main.c
+++ SmartClock-HID/main.c
@@ -34,110 +34,47 @@ publish any hardware using these IDs! This is for demonstration only!
 /* ----------------------------- USB interface ----------------------------- */
 /* ------------------------------------------------------------------------- */
 
-PROGMEM const char usbHidReportDescriptor[118] = { /* USB report descriptor, size must match usbconfig.h */
-    0x05, 0x01,        // USAGE_PAGE (Generic Desktop)
-    0x09, 0x02,        // USAGE (Mouse)
-    0xa1, 0x01,        // COLLECTION (Application)
-    0x09, 0x02,        //   USAGE (Mouse)
-    0xa1, 0x02,        //   COLLECTION (Logical)
-    0x09, 0x01,        //     USAGE (Pointer)
-    0xa1, 0x00,        //     COLLECTION (Physical)
-    // ------------------------------  Buttons
-    0x05, 0x09,        //       USAGE_PAGE (Button)
-    0x19, 0x01,        //       USAGE_MINIMUM (Button 1)
-    0x29, 0x05,        //       USAGE_MAXIMUM (Button 5)
-    0x15, 0x00,        //       LOGICAL_MINIMUM (0)
-    0x25, 0x01,        //       LOGICAL_MAXIMUM (1)
-    0x75, 0x01,        //       REPORT_SIZE (1)
-    0x95, 0x05,        //       REPORT_COUNT (5 Buttons)
-    0x81, 0x02,        //       INPUT (Data,Var,Abs)
-    // ------------------------------  Padding
-    0x75, 0x03,        //       REPORT_SIZE (8-5buttons 3)
-    0x95, 0x01,        //       REPORT_COUNT (1)
-    0x81, 0x03,        //       INPUT (Cnst,Var,Abs)
-    // ------------------------------  X,Y position
-    0x05, 0x01,        //       USAGE_PAGE (Generic Desktop)
-    0x09, 0x30,        //       USAGE (X)
-    0x09, 0x31,        //       USAGE (Y)
-    0x15, 0x81,        //       LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,        //       LOGICAL_MAXIMUM (127)
-    0x75, 0x08,        //       REPORT_SIZE (8)
-    0x95, 0x02,        //       REPORT_COUNT (2)
-    0x81, 0x06,        //       INPUT (Data,Var,Rel)
-    0xa1, 0x02,        //       COLLECTION (Logical)
-    // ------------------------------  Vertical wheel res multiplier
-    0x09, 0x48,        //         USAGE (Resolution Multiplier)
-    0x15, 0x00,        //         LOGICAL_MINIMUM (0)
-    0x25, 0x01,        //         LOGICAL_MAXIMUM (1)
-    0x35, 0x01,        //         PHYSICAL_MINIMUM (1)
-    0x45, 0x01,        //         PHYSICAL_MAXIMUM (1)
-    0x75, 0x02,        //         REPORT_SIZE (2)
-    0x95, 0x01,        //         REPORT_COUNT (1)
-    0xa4,              //         PUSH
-    0xb1, 0x02,        //         FEATURE (Data,Var,Abs)
-    // ------------------------------  Vertical wheel
-    0x09, 0x38,        //         USAGE (Wheel)
-    0x15, 0x81,        //         LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,        //         LOGICAL_MAXIMUM (127)
-    0x35, 0x00,        //         PHYSICAL_MINIMUM (0)        - reset physical
-    0x45, 0x00,        //         PHYSICAL_MAXIMUM (0)
-    0x75, 0x08,        //         REPORT_SIZE (8)
-    0x81, 0x06,        //         INPUT (Data,Var,Rel)
-    0xc0,              //       END_COLLECTION
-    0xa1, 0x02,        //       COLLECTION (Logical)
-    // ------------------------------  Horizontal wheel res multiplier
-    0x09, 0x48,        //         USAGE (Resolution Multiplier)
-    0xb4,              //         POP
-    0xb1, 0x02,        //         FEATURE (Data,Var,Abs)
-    // ------------------------------  Padding for Feature report
-    0x35, 0x00,        //         PHYSICAL_MINIMUM (0)        - reset physical
-    0x45, 0x00,        //         PHYSICAL_MAXIMUM (0)
-    0x75, 0x04,        //         REPORT_SIZE (4)
-    0xb1, 0x03,        //         FEATURE (Cnst,Var,Abs)
-    // ------------------------------  Horizontal wheel
-    0x05, 0x0c,        //         USAGE_PAGE (Consumer Devices)
-    0x0a, 0x38, 0x02,  //         USAGE (AC Pan)
-    0x15, 0x81,        //         LOGICAL_MINIMUM (-127)
-    0x25, 0x7f,        //         LOGICAL_MAXIMUM (127)
-    0x75, 0x08,        //         REPORT_SIZE (8)
-    0x81, 0x06,        //         INPUT (Data,Var,Rel)
-    0xc0,              //       END_COLLECTION
-    0xc0,              //     END_COLLECTION
-    0xc0,              //   END_COLLECTION
-    0xc0               // END_COLLECTION
+PROGMEM const char usbHidReportDescriptor[52] = { /* USB report descriptor, size must match usbconfig.h */
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                    // USAGE (Mouse)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xA1, 0x00,                    //   COLLECTION (Physical)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM
+    0x29, 0x03,                    //     USAGE_MAXIMUM
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x05,                    //     REPORT_SIZE (5)
+    0x81, 0x03,                    //     INPUT (Const,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x38,                    //     USAGE (Wheel)
+    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+    0x25, 0x7F,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+    0xC0,                          //   END_COLLECTION
+    0xC0,                          // END COLLECTION
 };
-
-
- //
- // Wheel Mouse - simplified version
- //
- // Input report - 5 bytes
- //
- //     Byte | D7      D6      D5      D4      D3      D2      D1      D0
- //    ------+---------------------------------------------------------------------
- //      0   |  0       0       0    Forward  Back    Middle  Right   Left (Button)
- //      1   |                             X
- //      2   |                             Y
- //      3   |                       Vertical Wheel
- //      4   |                    Horizontal (Tilt) Wheel
- //
- // Feature report - 1 byte
- //
- //     Byte | D7      D6      D5      D4   |  D3      D2  |   D1      D0
- //    ------+------------------------------+--------------+----------------
- //      0   |  0       0       0       0   |  Horizontal  |    Vertical
- //                                             (Resolution multiplier)
- //
- // Reference
- //    http://www.microchip.com/forums/m391435.aspx
- //
- 
+/* This is the same report descriptor as seen in a Logitech mouse. The data
+ * described by this descriptor consists of 4 bytes:
+ *      .  .  .  .  . B2 B1 B0 .... one byte with mouse button states
+ *     X7 X6 X5 X4 X3 X2 X1 X0 .... 8 bit signed relative coordinate x
+ *     Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0 .... 8 bit signed relative coordinate y
+ *     W7 W6 W5 W4 W3 W2 W1 W0 .... 8 bit signed relative coordinate wheel
+ */
 typedef struct{
     uchar   buttonMask;
     char    dx;
     char    dy;
     char    dWheel;
-	char	dPan;//Horizontal Wheel
 }report_t;
 
 static report_t reportBuffer;
@@ -145,6 +82,7 @@ static int      sinus = 7 << 6, cosinus = 0;
 static uchar    idleRate;   /* repeat rate for keyboards, never used for mice */
 
 static signed char delta = 0;
+static unsigned char interruptCount = 0;
 
 
 /* The following function advances sin/cos by a fixed angle
@@ -168,7 +106,6 @@ static void resetReportBuffer(report_t* r){
 	r->dx = 0;
 	r->dy = 0;
 	r->dWheel = 0;
-	r->dPan = 0;
 }
 
 /* ------------------------------------------------------------------------- */
@@ -202,16 +139,26 @@ usbRequest_t    *rq = (void *)data;
 
 ISR(INT1_vect)//ロリコンA変化
 {
-	if(delta != 0) return;
-	_delay_ms(1);   //チャタリング防止
-	cbi(EIFR,INTF1); //チャタリング防止の後で、割り込みフラグをクリア
-	if (bit_is_set(PIND,3)) { // エンコーダ出力AがやっぱりHighのとき
-		if (bit_is_clear(PIND,5)) { // エンコーダ出力BがLowのとき
-				delta++; //時計回り
-			} else {  // エンコーダ出力BがHighのとき
-				delta--; //反時計回り
+	//delta++;
+	interruptCount++;
+	if(interruptCount > 0) tbi(PORTD,PD6);//cbi(GIMSK,INT1);//INT1割り込みやめて
+	if(delta == 0) {
+		for(int i = 0; i < 16; i++) {
+			_delay_us(100);   //チャタリング防止
+			wdt_reset();
+			usbPoll();
+		}
+		cbi(EIFR,INTF1); //チャタリング防止の後で、割り込みフラグをクリア
+		if (bit_is_set(PIND,3)) { // エンコーダ出力AがやっぱりHighのとき
+			
+			if (bit_is_clear(PIND,5)) { // エンコーダ出力BがLowのとき
+					delta++; //時計回り
+				} else {  // エンコーダ出力BがHighのとき
+					delta--; //反時計回り
+			}
 		}
 	}
+	//cbi(PORTD,PD6);
 }
 
 int __attribute__((noreturn)) main(void)
@@ -227,7 +174,7 @@ int __attribute__((noreturn)) main(void)
 	//Rotary init
 	cbi(DDRD,PD3);//PD3入力
 	cbi(DDRD,PD5);//PD5入力
-	sbi(PORTD,PD3);//PD3内部プルアップ
+	cbi(PORTD,PD3);//PD3内部プルアップ **無効**
 	sbi(PORTD,PD5);//PD5内部プルアップ
 	
 	sbi(MCUCR, ISC11);
@@ -261,10 +208,9 @@ int __attribute__((noreturn)) main(void)
 	//unsigned char j = 0;
     for(;;){                /* main event loop */
 		//USB loop start
+		cbi(GIMSK,INT1);//INT1割り込みやめて
         DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
         wdt_reset();
-		
-		cbi(GIMSK,INT1);//INT1割り込みやめて
         usbPoll();
 		sbi(GIMSK,INT1);//INT1割り込み許可
 		
@@ -273,8 +219,8 @@ int __attribute__((noreturn)) main(void)
             /* called after every poll of the interrupt endpoint */
             //advanceCircleByFixedAngle();
 			
-			if(bit_is_set(PIND,3)) sbi(PORTD,PD6);
-			else cbi(PORTD,PD6);
+			//if(bit_is_set(PIND,3)) sbi(PORTD,PD6);
+			//else cbi(PORTD,PD6);
 			
             DBG1(0x03, 0, 0);   /* debug output: interrupt report prepared */
 			//j++;
@@ -286,7 +232,8 @@ int __attribute__((noreturn)) main(void)
             usbSetInterrupt((void *)&reportBuffer, sizeof(reportBuffer));
 			//resetReportBuffer(&reportBuffer);
 			delta=0;
-			//tbi(PORTB,PB0);
+			interruptCount = 0;
+			//tbi(PORTD,PD6);
 			sbi(GIMSK,INT1);//INT1割り込み許可
         }
 		//USB loop end
--- SmartClock-HID/usbconfig.h
+++ SmartClock-HID/usbconfig.h
@@ -235,7 +235,7 @@ section at the end of this file).
  * with libusb: 0x16c0/0x5dc.  Use this VID/PID pair ONLY if you understand
  * the implications!
  */
-#define USB_CFG_DEVICE_VERSION  0x01, 0x01
+#define USB_CFG_DEVICE_VERSION  0x02, 0x01
 /* Version number of the device: Minor number first, then major number.
  */
 #define USB_CFG_VENDOR_NAME     'w', 'a', 'r', 'a', 'r', 'y', 'o'
@@ -276,7 +276,7 @@ section at the end of this file).
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    118
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    52
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
